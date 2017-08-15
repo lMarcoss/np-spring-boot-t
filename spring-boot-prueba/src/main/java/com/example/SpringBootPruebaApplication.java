3588bf518e42925ef5b374b4593dcd5ec4a18644
Leo <markos_193@hotmail.com>
2017-08-14 23:52:02 -0500

    proyecto inicial de spring boot

---
+++ spring-boot-prueba/src/main/java/com/example/SpringBootPruebaApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPruebaApplication.class, args);
+	}
+}
